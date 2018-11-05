7a8df862c03f29ab44bf74bb18bcca44790b488f
Vexrax <joshuaf_111@live.com>
2018-11-04 20:07:35 -0500

    Size is now constant, small html fixes.

--- main.js
+++ main.js
@@ -7,20 +7,32 @@ const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
+
 app.on('ready', function ()
 {
+
     mainWindow = new BrowserWindow();
+
+    mainWindow.setMaximumSize(1200, 800);
+    mainWindow.setMinimumSize(1200, 800);
+    //mainWindow.setResizable(false);
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'HTML', 'mainWindow.html'),
         protocol: 'file:',
-        slashes:true
+        slashes:true,
+        frame: false,
+
     }));
     mainWindow.on('closed', function(){
         app.quit();
     });
+
+
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
 });
+
 
 const mainMenuTemplate = [];
 
@@ -31,6 +43,7 @@ if(process.env.NODE_ENV !== 'production')
         submenu: [
             {
                 role: 'reload'
+
             },
             {
                 label: 'Toggle DevTools',
@@ -42,3 +55,9 @@ if(process.env.NODE_ENV !== 'production')
         ]
     });
 }
+
+
+
+
+
+
