1da89376cb0fc5787c81a65598139b1bdd6cf62f
Vexrax <joshuaf_111@live.com>
2018-11-18 18:11:42 -0500

    Adding in champgrid template

---
+++ src/renderer/app/ChampGridForm.vue
@@ -0,0 +1,45 @@
+<script>
+    import Vue from 'vue';
+    import { MenuItemGroup } from 'element-ui';
+
+    export default {
+        name: 'champion-grid-form',
+        computed: {
+            runes: {
+                get() {
+                    return this.$store.state.runes.state.Runes;
+                },
+                set(value) {
+                    this.$store.commit('runes/setRunes', value);
+                },
+            },
+            KeystoneTree: {
+                get() {
+                    return this.$store.state.runes.state.KeystoneTree;
+                },
+                set(value) {
+                    this.$store.commit('runes/SetKeystoneTree', value);
+                },
+            },
+            SecondaryTree: {
+                get() {
+                    return this.$store.state.runes.state.SecondTree;
+                },
+                set(value) {
+                    this.$store.commit('runes/SetSecondaryTree', value);
+                },
+            },
+
+        },
+    }
+</script>
+
+<template>
+    
+</template>
+
+
+
+<style scoped>
+
+</style>
