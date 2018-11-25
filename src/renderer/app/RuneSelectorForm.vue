b9161a573c0e6a38a268464bb1ba25d0ef601c9b
Vexrax <joshuaf_111@live.com>
2018-11-24 20:01:26 -0500

    Added logic into rune trees

--- src/renderer/app/RuneSelectorForm.vue
+++ src/renderer/app/RuneSelectorForm.vue
@@ -3,18 +3,17 @@
     import ElContainer from "element-ui/packages/container/src/main";
     import RuneJson from '../staticJSONs/RuneData'
 
-    // const DominationMatrix =
-    //     [
-    //         [8112, 8124, 8124, 9923],
-    //         [8126, 8139, 8143],
-    //         [8136, 8120, 8138],
-    //         [8135,8134,8105,8106]
-    //     ];
+    var KeyStoneMatrix = [-1, -1, -1, -1];
+    var SecondaryTreeMatrix = [-1, -1, -1];
+    var selectedSecondaryRunes = 0;
+
+
     export default {
         components: {ElContainer},
         data() {
             return {
                 RuneData: RuneJson,
+                componentKey: 0,
             }
         },
         methods: {
@@ -22,9 +21,14 @@
             {
                 if(isPrimary){
                     this.$store.commit('runes/SetKeystoneTree', TreeId);
+                    //reset the selected tree matrix
+                    KeyStoneMatrix = [-1, -1, -1, -1];
                 }
                 else{
                     this.$store.commit('runes/SetSecondaryTree', TreeId);
+                    //reset the selected tree matrix
+                    SecondaryTreeMatrix = [-1, -1, -1];
+                    selectedSecondaryRunes = 0;
                 }
 
             },
@@ -44,8 +48,47 @@
                     return "background-color: black ;";
                 }
                 return "background-color: #181818;";
+            },
+            IsRuneSelected: function(id, isKeyStone)
+            {
+                if((KeyStoneMatrix.includes(id) && isKeyStone) || (SecondaryTreeMatrix.includes(id) && !isKeyStone))
+                {
+                    return '';
+                }
+                return '?image=e_grayscale&v=1';
+            },
+            onRuneClick: function(id, row, isKeyStone)
+            {
+                if(isKeyStone)
+                {
+                    //if selected unselect rune
+                    if(KeyStoneMatrix.includes(id))
+                    {
+                        KeyStoneMatrix[row] = -1;
+                        this.componentKey += 1;
+                        return;
+                    }
+                    KeyStoneMatrix[row] = id;
+                }
+                else
+                {
+                    //if selected unselect rune
+                    if(SecondaryTreeMatrix.includes(id))
+                    {
+                        SecondaryTreeMatrix[row] = -1;
+                        this.componentKey += 1;
+                        selectedSecondaryRunes -= 1;
+                        return;
+                    }
+                    if(selectedSecondaryRunes <= 1) //you can only have 2 secondary runes 
+                    {
+                        SecondaryTreeMatrix[row] = id;
+                        selectedSecondaryRunes += 1;
+                    }
+                }
+                //used to force re-render since vue doesnt auto re-render methods.
+                this.componentKey += 1;
             }
-
 
         },
         computed:{
@@ -94,7 +137,7 @@
 
 <template>
     <el-container>
-        <div class="runeContainer">
+        <div class="runeContainer" >
             <div class="row">
                 <img src="../icons/runes/8000.png" v-on:click="SetTree(0,true)" v-bind:style="KeyStoneBackGroundColor(0)"/>
                 <img src="../icons/runes/8100.png" v-on:click="SetTree(1,true)" v-bind:style="KeyStoneBackGroundColor(1)"/>
@@ -102,18 +145,19 @@
                 <img src="../icons/runes/8300.png" v-on:click="SetTree(3,true)" v-bind:style="KeyStoneBackGroundColor(3)"/>
                 <img src="../icons/runes/8400.png" v-on:click="SetTree(4,true)" v-bind:style="KeyStoneBackGroundColor(4)"/>
             </div>
-
-            <div class="runerow" id="row1">
-                <img class="rune" v-for="img in KeyStoneRowOneJSON" v-bind:src="img['icon']" v-bind:id="img['id']"/>
-            </div>
-            <div class="runerow" id="row2">
-                <img class="rune" v-for="img in KeyStoneRowTwoJSON" v-bind:src="img['icon']" v-bind:id="img['id']"/>
-            </div>
-            <div class="runerow" id="row3">
-                <img class="rune" v-for="img in KeyStoneRowThreeJSON" v-bind:src="img['icon']" v-bind:id="img['id']"/>
-            </div>
-            <div class="runerow" id="row4">
-                <img class="rune" v-for="img in KeyStoneRowFourJSON" v-bind:src="img['icon']" v-bind:id="img['id']"/>
+            <div :key="componentKey">
+                <div class="runerow" id="row1" >
+                    <img class="rune" v-for="img in KeyStoneRowOneJSON" v-bind:src="img['icon'] + IsRuneSelected(img['id'], true)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 0, true)"/>
+                </div>
+                <div class="runerow" id="row2">
+                    <img class="rune" v-for="img in KeyStoneRowTwoJSON" v-bind:src="img['icon'] + IsRuneSelected(img['id'], true)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 1, true)" />
+                </div>
+                <div class="runerow" id="row3">
+                    <img class="rune" v-for="img in KeyStoneRowThreeJSON" v-bind:src="img['icon'] + IsRuneSelected(img['id'], true)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 2, true)"/>
+                </div>
+                <div class="runerow" id="row4">
+                    <img class="rune" v-for="img in KeyStoneRowFourJSON" v-bind:src="img['icon'] + IsRuneSelected(img['id'], true)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 3, true)"/>
+                </div>
             </div>
         </div>
         <div class="runeContainer">
@@ -125,13 +169,13 @@
                 <img src="../icons/runes/8400.png" v-on:click="SetTree(4, false)" v-bind:style="SecondaryTreeBackGroundColor(4)"/>
             </div>
             <div class="runerow" id="row1_2">
-                <img class="rune" v-for="img in SecondaryTreeRowOne" v-bind:src="img['icon']" v-bind:id="img['id']"/>
+                <img class="rune" v-for="img in SecondaryTreeRowOne" v-bind:src="img['icon'] + IsRuneSelected(img['id'], false)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 0, false)"/>
             </div>
             <div class="runerow" id="row2_2">
-                <img class="rune" v-for="img in SecondaryTreeRowTwo" v-bind:src="img['icon']" v-bind:id="img['id']"/>
+                <img class="rune" v-for="img in SecondaryTreeRowTwo" v-bind:src="img['icon'] + IsRuneSelected(img['id'], false)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 1, false)"/>
             </div>
             <div class="runerow" id="row3_2">
-                <img class="rune" v-for="img in SecondaryTreeRowThree" v-bind:src="img['icon']" v-bind:id="img['id']"/>
+                <img class="rune" v-for="img in SecondaryTreeRowThree" v-bind:src="img['icon'] + IsRuneSelected(img['id'], false)" v-bind:id="img['id']" v-on:click="onRuneClick(img['id'], 2, false)"/>
             </div>
         </div>
     </el-container>
